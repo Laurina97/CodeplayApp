8f722e240468cb127cc5241ca67c3c0eb2d0dbbf
Liza Pettersson <liza_pettersson@hotmail.com>
2017-02-06 12:27:47 +0100

    test

---
+++ app/src/main/java/se/codemill/codeplay/Codeplay.java
@@ -0,0 +1,13 @@
+package se.codemill.codeplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Codeplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_codeplay);
+    }
+}
